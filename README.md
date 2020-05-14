29b9332bb49bea3d228b0feb3e4f7bc0d059e75c
Gousemoodhin Nadaf <nadafgouse5@gmail.com>
2020-05-13 21:40:12 -0400

    Implement LRU Cache
    - If element is new, add it to cache.
    - If element is already present, move it to the front.
    - If the cache is full, remove the last element from cache.
    - Linked List + Hash Table, gives time complexity O(1) (contant time).

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Cache_LRU
 Cache Implementation (LRU cache)
+
+- LRU(Least Recently Used Cache) is an eviction policy
+- Size of the cache is fixed, when the cache is full, it removes the least recently used element
---
+++ cache.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <list>
+#include<unordered_map>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;  // to measure time.
+
+
+class CacheLRU {
+  int CacheSize_;
+  list<int> CacheMemory;
+  unordered_map<int, list<int>::iterator> ref_hashtable_;
+  public:
+    void CacheLRUFill(int element);
+    void DisplayCacheElements();
+    CacheLRU (int size) {
+      CacheSize_ = size;
+    }
+};
+
+/*
+  @ CacheLRUFill().
+  - If element is new, add it to cache.
+  - If element is already present, move it to the front.
+  - If the cache is full, remove the last element from cache.
+  - Linked List + Hash Table, gives time complexity O(1) (contant time).
+*/
+void CacheLRU::CacheLRUFill(int element) {
+  list<int>::iterator it = find(CacheMemory.begin(), CacheMemory.end(), element);
+  if(ref_hashtable_.find(element)== ref_hashtable_.end()) {
+    if(ref_hashtable_.size() == CacheSize_){
+      int pop_element = CacheMemory.back();
+      CacheMemory.pop_back();
+      ref_hashtable_.erase(pop_element);
+    }
+  } else {
+    CacheMemory.erase(ref_hashtable_.find(element)->second);
+  }
+  CacheMemory.push_front(element);
+  ref_hashtable_[element] = CacheMemory.begin();
+}
+
+void CacheLRU::DisplayCacheElements() {
+  for(auto it = CacheMemory.begin(); it != CacheMemory.end(); it++) {
+    cout << *it <<" ";
+  }
+  cout << endl;
+}
+
+
+int main() {
+  // create instance of class.
+  CacheLRU cache_lru(3);
+  
+  // Add cache elements. 
+  auto start = high_resolution_clock::now();
+  cache_lru.CacheLRUFill(4);
+  cache_lru.CacheLRUFill(5);
+  cache_lru.CacheLRUFill(6);
+  cache_lru.CacheLRUFill(5);
+  cache_lru.CacheLRUFill(4);
+  cache_lru.CacheLRUFill(1);
+  cache_lru.CacheLRUFill(3);
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+  // Display cache elements.
+  cache_lru.DisplayCacheElements();
+  return 0;
+}
